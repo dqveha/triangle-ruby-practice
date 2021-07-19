485773cca360bcc9625f97893bd7bb56605f700c
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-19 13:32:44 -0700

    Update triangle.rb with business logic
    Co-authored-by: Ben Pawlik <james.benjamin.pawlik@gmail.com>

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,2 +1,19 @@
+class Check_Triangle
+  attr_accessor(:side1, :side2, :side3)
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
 def triangle
+  if @side1 === @side2 && @side2 === @side3
+    return "This is an equilateral triangle!"
+  elsif ((side1 !== side2) && (side1 !== side3) && (side2 !== side3))
+    return "This is a scalene triangle!"
+  elsif (side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)
+    return "YOU'RE A SQUARE WITHOUT A FOURTH SIDE."
+  else
+    return "This is an isosceles triangle!"
 end
