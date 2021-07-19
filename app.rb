cd63b0a7327fe2d91dbd6ea431eda72018eaff0e
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-19 15:07:23 -0700

    Have a fully functional triangle app within ruby
    Co-authored-by: Ben Pawlik <james.benjamin.pawlik@gmail.com>

--- app.rb
+++ app.rb
@@ -4,16 +4,23 @@ require('./lib/triangle')
 also_reload 'lib/**/*.rb'
 require 'pry'
 
-post '/' do
-  side1 = params[:side1]
-  side2 = params[:side2]
-  side3 = params[:side3]
-  is_triangle = Check_Triangle.new(side1, side2, side3)
-  @result = is_triangle.triangle()
+
+get '/' do
+  # side1 = params[:side1]
+  # side2 = params[:side2]
+  # side3 = params[:side3]
+  # @is_triangle = Check_Triangle.new(side1, side2, side3)
   erb(:triangle_input)
 end
 
-get '/result' do
+post '/result' do
+  side1 = params[:side1]
+  @side1 = side1.to_i
+  side2 = params[:side2]
+  @side2 = side2.to_i
+  side3 = params[:side3]
+  @side3 = side3.to_i
+  is_triangle = Check_Triangle.new(@side1, @side2, @side3)
   @result = is_triangle.triangle()
   erb(:triangle_output)
 end
--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,12 +10,12 @@ class Check_Triangle
   def triangle
     if @side1 === @side2 && @side2 === @side3
       return "This is an equilateral triangle!"
-    elsif ((@side1 != @side2) && (@side1 != @side3) && (@side2 != @side3))
-      return  "This is a scalene triangle!"
+    elsif (((side1 === side2 && side2 != side3) && (side1 + side2 > side3)) || ((side2 === side3 && side3 != side1) && (side3 + side2 > side1)) || ((side3 === side1 && side1 != side2) && (side1 + side3 > side2)))
+      return "This is an isosceles triangle!"
     elsif (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1) || (@side1 + @side3 <= @side2)
       return "YOU'RE A SQUARE WITHOUT A FOURTH SIDE."
     else
-      return "This is an isosceles triangle!"
+      return "This is a scalene triangle!"
     end
   end
 end
