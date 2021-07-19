7c3196ceb9c5b7660edc9c2ea109bce096051e38
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-19 14:21:49 -0700

    Add specs with three tests pass, entering triangle sides outputs type
    Co-authored-by: Ben Pawlik <james.benjamin.pawlik@gmail.com>

--- app.rb
+++ app.rb
@@ -4,10 +4,16 @@ require('./lib/triangle')
 also_reload 'lib/**/*.rb'
 require 'pry'
 
-get '/' do
+post '/' do
+  side1 = params[:side1]
+  side2 = params[:side2]
+  side3 = params[:side3]
+  is_triangle = Check_Triangle.new(side1, side2, side3)
+  @result = is_triangle.triangle()
   erb(:triangle_input)
 end
 
 get '/result' do
+  @result = is_triangle.triangle()
   erb(:triangle_output)
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,12 +1,21 @@
-# require 'rspec'
-# require 'hello'
+require 'rspec'
+require 'triangle'
 
-# describe '#Hello' do
+describe '#Check_Triangle' do
+  it("should create an empty triangle object") do
+    is_triangle = Check_Triangle.new("side1","side2","side3")   
+    expect(is_triangle.side1).to(eq('side1'))
+  end
 
-#   describe('#hello_world') do
-#     it("says Hello World!") do
-#       hello = Hello.new()
-#       expect(hello.hello_world()).to(eq("Hello world!"))
-#     end
-#   end
-# end
+  it("should create a triangle object with side lengths") do
+    is_triangle = Check_Triangle.new(1, 2, 1)
+    expect(is_triangle.side1).to(eq(1))
+    expect(is_triangle.side2).to(eq(2))
+    expect(is_triangle.side3).to(eq(1))
+  end
+
+  it("should check the triangle type") do
+    is_triangle = Check_Triangle.new(1,2,2)
+    expect(is_triangle.triangle).to(eq('This is an isosceles triangle!'))
+  end
+end
